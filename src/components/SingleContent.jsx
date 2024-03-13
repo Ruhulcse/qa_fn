92ce10af060443d7da8b4eeb05b6bd63f357afe3
abircu <mdabircse4873@gmail.com>
2024-03-13 17:31:10 +0600

    button handle

--- src/components/SingleContent.jsx
+++ src/components/SingleContent.jsx
@@ -3,8 +3,24 @@ import axios from "axios";
 import { BiSolidDownArrowCircle, BiSolidUpArrowCircle } from "react-icons/bi";
 import "react-toastify/dist/ReactToastify.css";
 import baseUrl from "../config/config";
+import { useEffect, useState } from "react";
 
 function SingleContent({ singleQuestion, fetchQuestions }) {
+  // modifiaction
+  const [voteStatus, setVoteStatus] = useState({
+    upvote: singleQuestion.upvote,
+    downvote: singleQuestion.downvote,
+  });
+
+  const [voteMassage, setVoteMassage] = useState("");
+  useEffect(() => {
+    if (singleQuestion.upvote) {
+      setVoteMassage("you have already upvoted this question.");
+    } else if (singleQuestion.downvote) {
+      setVoteMassage("you have already downvoted this question.");
+    }
+  }, [singleQuestion]);
+
   const getData = async () => {
     const res = await axios.get("https://api.ipify.org/?format=json");
     return res.data.ip;
@@ -12,12 +28,16 @@ function SingleContent({ singleQuestion, fetchQuestions }) {
 
   const handleUpVote = async (question) => {
     try {
+      if (voteStatus.upvote) {
+        setVoteMassage("you have already upvoted this question.");
+      }
       const ipAddress = await getData();
       const payload = {
         voteType: "upvote",
         voterIP: ipAddress,
       };
       await axios.post(`${baseUrl}/questions/${question._id}/vote`, payload);
+      setVoteStatus({ ...voteStatus, upvote: true });
       fetchQuestions(); // Fetch the latest questions data after voting
     } catch (error) {
       console.log(error);
@@ -26,12 +46,16 @@ function SingleContent({ singleQuestion, fetchQuestions }) {
 
   const handleDownVote = async (question) => {
     try {
+      if (voteStatus.downvote) {
+        setVoteMassage("you have already downvoted this question");
+      }
       const ipAddress = await getData();
       const payload = {
         voteType: "downvote",
         voterIP: ipAddress,
       };
       await axios.post(`${baseUrl}/questions/${question._id}/vote`, payload);
+      setVoteStatus({ ...voteStatus, downvote: true });
       fetchQuestions(); // Fetch the latest questions data after voting
     } catch (error) {
       console.log(error);
@@ -51,7 +75,9 @@ function SingleContent({ singleQuestion, fetchQuestions }) {
       </div>
       <div className="flex items-center gap-4 mt-4">
         <button
-          className="flex items-center justify-center gap-2 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out"
+          className={`flex items-center justify-center gap-2 ${
+            voteStatus.upvote ? "bg-green-400" : "bg-blue-800 hover:bg-blue-500"
+          } text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out`}
           onClick={() => handleUpVote(singleQuestion)}
         >
           <BiSolidUpArrowCircle className="text-3xl" />
@@ -59,12 +85,15 @@ function SingleContent({ singleQuestion, fetchQuestions }) {
         </button>
         <h1>{singleQuestion.total_vote}</h1>
         <button
-          className="flex items-center justify-center gap-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out"
+          className={`flex items-center justify-center gap-2 ${
+            voteStatus.downvote ? "bg-red-400" : "bg-red-500 hover:bg-red-600"
+          } text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out`}
           onClick={() => handleDownVote(singleQuestion)}
         >
           <BiSolidDownArrowCircle className="text-3xl" />
         </button>
       </div>
+      {voteMassage && <p className="text-red-500">{voteMassage}</p>}
     </div>
   );
 }
